de6154846fe0b6698252719c39657bf23692cb36
ISLOMIDDIN98 <najmiddinovislomiddin2gmail.com>
2022-01-25 23:08:58 +0900

    add select-or-reject styles

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+let container = document.querySelector(".menu-btn");
+
+container.addEventListener("click", function(){
+  // document.querySelectorAll('.menu-btn').forEach(function(menubtn){
+    container.classList.toggle("menu-btn-active");
+  // });
+
+});
