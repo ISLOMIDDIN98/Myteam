3668ace42a57f5c44450c7170ffca912728a6361
ISLOMIDDIN98 <najmiddinovislomiddin2gmail.com>
2022-02-15 16:31:02 +0900

    add about main jc

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
+"use strict";
+// const container = document.querySelector(".menu-btn");
+// const elhidden = document.querySelector(".directors-info-hidden");
 
-let container = document.querySelector(".menu-btn");
+// container.addEventListener("click", function(){
+//   // document.querySelectorAll('.menu-btn').forEach(function(menubtn){
+//     container.classList.toggle("menu-btn-active");
+//     elhidden.classList.toggle("hidden");
+//   // });
 
-container.addEventListener("click", function(){
-  // document.querySelectorAll('.menu-btn').forEach(function(menubtn){
-    container.classList.toggle("menu-btn-active");
-  // });
+// });
 
+
+
+const elhidden = document.querySelectorAll(".directors-info-hidden");
+const eltogger = document.querySelectorAll(".plus-toggler");
+const elplusImg = document.querySelectorAll(".plus-img");
+const elbodyScroll = document.querySelector("body");
+
+eltogger.forEach((Element, index) => {
+  Element.addEventListener("click", function () {
+    Element.classList.toggle("toggled-state");
+    elhidden[index].classList.toggle("hidden");
+    elplusImg[index].classList.toggle("plus-toggled");
+  });
 });
